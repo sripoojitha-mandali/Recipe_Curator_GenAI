c493bc7ad03761914b1f419fc1f316c6e940686a
Sri Poojitha Mandali <114782541+sripoojitha-mandali@users.noreply.github.com>
2024-12-03 13:55:09 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import pinecone
 
 # Initialize Pinecone with just the API key
-pinecone.init(api_key='b1d78acd-4331-4743-9967-8bcf0e8eb197')
+pinecone.init(api_key='xxx')
 
 # Print the list of environments
 print("Available Pinecone Environments:")
